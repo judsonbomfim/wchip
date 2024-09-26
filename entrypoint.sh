a7fd370dfb00d61246977a6a6e0d52bf3a50c49a
Judson Bomfim <judsonbomfim@gmail.com>
2024-09-26 20:02:37 -0300

    Ajustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,11 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
+nohup --config gunicorn_config.py core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
 nohup celery -A core worker -B --loglevel=info
+
+
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+gunicorn --config gunicorn_config.py core.wsgi:application
