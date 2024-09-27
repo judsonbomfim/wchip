3ef1caa842a41c45e997744b3d2dc7753a6fd366
Judson Bomfim <judsonbomfim@gmail.com>
2024-09-27 09:53:37 -0300

    Sem SSL - Restaurar ao anterior

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-gunicorn --config gunicorn_config.py core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
+nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
 nohup celery -A core worker -B --loglevel=info
