260887688036591f34832093838522c35e8e822a
Judson Bomfim <judsonbomfim@gmail.com>
2024-09-28 19:43:39 -0300

    Ajustes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@
 # nohup gunicorn core.wsgi:application --bind 0.0.0.0:8000 --log-level=info &
 # nohup celery -A core worker -B --loglevel=info
 
-
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
