10cc3f4239b0376d4adb4b18e5de75d9876031f4
Judson Bomfim <judsonbomfim@gmail.com>
2024-10-10 14:59:14 -0300

    Modelos de e-mail

--- apps/send_email/urls.py
+++ apps/send_email/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path('enviar/', views.SendEmail.send_email, name=''),
     path('enviar/<int:id>', views.send_email, name='send_email'),
     path('enviar_esims', views.send_email_esims, name='send_email_esims'),
+    path('visualizar', views.visualizar, name='visualizar'),
 ]
--- apps/send_email/views.py
+++ apps/send_email/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from apps.send_email.tasks import send_email_sims
@@ -12,3 +12,7 @@ def send_email(request,id):
 @login_required(login_url='/login/')
 def send_email_esims():
     send_email_sims.delay()
+    
+@login_required(login_url='/login/')
+def visualizar(request):
+    return render(request, 'painel/emails/send_email.html')
