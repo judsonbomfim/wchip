6bdf52c90815991a434a6946aebc6549bb4af44b
Judson Bomfim <judsonbomfim@gmail.com>
2024-10-10 16:23:19 -0300

    AJuste

--- core/settings.py
+++ core/settings.py
@@ -175,7 +175,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Default primary key field type
