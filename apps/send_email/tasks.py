ffafaa7f99617002626d6021fa15779fdeadaa42
Judson Bomfim <falecom@judsonbomfim.com>
2026-03-31 16:20:33 -0300

    Correções

--- apps/send_email/tasks.py
+++ apps/send_email/tasks.py
@@ -38,7 +38,7 @@ def send_email_sims(id=None):
         try: type_sim = order.id_sim.type_sim
         except: continue            
         countries = order.countries
-        operation = order.id_sim.operator
+        operator = order.id_sim.operator
                 
         context = {
             'url_site': url_site,
@@ -53,7 +53,7 @@ def send_email_sims(id=None):
             'type_sim': type_sim,
             'countries': countries,
             'tracking': order.tracking,
-            'operation': operation,
+            'operator': operator,
         }
         html_content = render_to_string('painel/emails/send_email.html', context)
         text_content = strip_tags(html_content)
