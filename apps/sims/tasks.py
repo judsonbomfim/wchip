b05ee10ee414ae58c3df8415915ea7ddf30a4dd3
Judson Bomfim <falecom@judsonbomfim.com>
2026-04-19 10:52:52 -0300

    Ajuste na ativação do AR

--- apps/sims/tasks.py
+++ apps/sims/tasks.py
@@ -645,7 +645,7 @@ def simActivateAR(id=None):
     
     # Selecionar pedidos
     if id is None:
-        orders_all = Orders.objects.filter(order_status='AA', id_sim=351, activation_date__lte=tomorrow)
+        orders_all = Orders.objects.filter(order_status='AA', activation_date__lte=tomorrow)
         if not orders_all.exists():
             logger.info(f'>>>>>>>>>> Nenhum pedidos encontrados para ativação AR')
             return
